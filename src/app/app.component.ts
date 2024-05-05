93cc629559c7775c5c9e5fd375eaa19d9f437d22
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-05-04 20:52:41 -0500

    commit initial.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ep2023-2-upc';
+  options=[
+    {path: '/home', title: 'Home'},
+    {path: '/store/bundles', title: 'Bundles'},
+  ]
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./public/pages/home/home.component";
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
+import {BundleManagmentComponent} from "./store/pages/bundle-managment/bundle-managment.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'store/bundles', component: BundleManagmentComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path: '**', component: PageNotFoundComponent}
+];
